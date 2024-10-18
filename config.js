7f95f326070ce57de2bf9793a2ce56ccaffb422f
Strictop <91557750+Strictop@users.noreply.github.com>
2024-10-18 14:40:14 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     app: {
         px: '+', // Le prefix exemple +help *help ta capter le prefix avant la commande...
-        owners: '960593732862902312', '705403970771746828', '1064654693231513650', // Ici tu te met owner (Que toi pas tes meilleurs collègue...)
+        owners: '960593732862902312', // Ici tu te met owner (Que toi pas tes meilleurs collègue...)
         funny: '960593732862902312',  // Ici tu te met owner (Que toi pas tes meilleurs collègue...)
         color: '#6495ED', // Couleur en HEX | https://htmlcolorcodes.com/fr/selecteur-de-couleur/
         footer: 'Strictop-Protect', // Quand tu fais +help par exemple y'a un texte en bas de l'embed...
